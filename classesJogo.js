939862685680bdc50a2da89fff9a9f18c89f3ee5
felipesilva017 <felipesilva017@gmail.com>
2024-01-22 20:22:48 -0300

    first commit

---
+++ classesJogo.js
@@ -0,0 +1,36 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case 'mago':
+        ataque = 'usou magia';
+        break;
+      case 'guerreiro':
+        ataque = 'usou espada';
+        break;
+      case 'monge':
+        ataque = 'usou artes marciais';
+        break;
+      case 'ninja':
+        ataque = 'usou shuriken';
+        break;
+      default:
+        ataque = 'realizou um ataque';
+        break;
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const mago = new Heroi('Merlin', 100, 'mago');
+const guerreiro = new Heroi('Conan', 35, 'guerreiro');
+
+mago.atacar(); 
+guerreiro.atacar(); 
